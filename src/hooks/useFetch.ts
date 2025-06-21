761237f5264363ec0ab37bd7e8431a45159263a0
Yuvika <yuvika@catalyst.sh>
2025-06-22 00:04:59 +0530

    feat: implement pollInterval functionality after PR review changes

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,48 +1,62 @@
 import { useEffect, useState } from "react";
 import api from "../services/api.service";
 
-type usefetchArgs={
-    url: string;
-    autoFetch?: boolean;
-    transformer?: (data: any) => any;
-    onSuccess?: (data: any) => any;
-    onError?: (error: any) => any;
-}
+type usefetchArgs = {
+  url: string;
+  autoFetch?: boolean;
+  transformer?: (data: any) => any;
+  onSuccess?: (data: any) => any;
+  onError?: (error: any) => any;
+  pollInterval?: number;
+};
 
-const useFetch = ({url, autoFetch = true, transformer, onSuccess, onError}:usefetchArgs) => {
-    const [data, setData] = useState<any>(null);
-    const [error, setError] = useState<any>("");
-    const [loading, setLoading] = useState<boolean>(false);
+const useFetch = ({
+  url,
+  autoFetch = true,
+  transformer,
+  onSuccess,
+  onError,
+  pollInterval,
+}: usefetchArgs) => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchResponse = async () => {
-        setLoading(true);
-        try {
-            const response = await api.get(url);
-            const result = transformer ? transformer(response.data) : response.data;
-            setData(result);
-            if(onSuccess){
-                onSuccess(result);
-            }
-        } catch (e:any) {
-            setError(e);
-            if(onError){
-               onError(e);
-            }
-        }
-        finally {
-            setLoading(false);
-        }
+  const fetchResponse = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get(url);
+      const result = transformer ? transformer(response.data) : response.data;
+      setData(result);
+      if (onSuccess) {
+        onSuccess(result);
+      }
+    } catch (e: any) {
+      setError(e);
+      if (onError) {
+        onError(e);
+      }
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        if (autoFetch) {
-            fetchResponse();
-        }
-    }, [url]);
-
-    return {
-        data, error, loading,refetch:fetchResponse
+  useEffect(() => {
+    if (autoFetch) {
+      fetchResponse();
     }
-}
+    if (pollInterval) {
+      const val = setInterval(() => fetchResponse(), pollInterval);
+      return () => clearInterval(val);
+    }
+  }, [url]);
+
+  return {
+    data,
+    error,
+    loading,
+    refetch: fetchResponse,
+  };
+};
 
 export default useFetch;
