1645cc4093e6e0e61246b53414ba642304759ecd
Yuvika <yuvika@catalyst.sh>
2025-06-23 21:25:26 +0530

    feat: add UI in App.tsx, setup placeholder API for testing, and implement pollInterval & retryCount logic

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import api from "../services/api.service";
 
 type usefetchArgs = {
@@ -8,6 +8,8 @@ type usefetchArgs = {
   onSuccess?: (data: any) => any;
   onError?: (error: any) => any;
   pollInterval?: number;
+  retryOnFailCount?: number;
+  retryOnFailInterval?: number;
 };
 
 const useFetch = ({
@@ -17,10 +19,25 @@ const useFetch = ({
   onSuccess,
   onError,
   pollInterval,
+  retryOnFailCount = 0,
+  retryOnFailInterval = 1000,
 }: usefetchArgs) => {
+    
   const [data, setData] = useState<any>(null);
   const [error, setError] = useState<any>("");
   const [loading, setLoading] = useState<boolean>(false);
+
+  //useRefs for pollInterval and retryCount-using useRef bcz there was no requirement of UI updates and need of re-rendering
+  const fetchSuccessful=useRef<number|null>(null);
+   const retryCount = useRef(0);
+
+   //cleanup function for fetchSuccessful
+  const clearPoll=()=>{
+    if(fetchSuccessful.current){
+      clearInterval(fetchSuccessful.current);
+      fetchSuccessful.current=null;
+    }
+  }
 
   const fetchResponse = async () => {
     setLoading(true);
@@ -28,13 +45,27 @@ const useFetch = ({
       const response = await api.get(url);
       const result = transformer ? transformer(response.data) : response.data;
       setData(result);
+      retryCount.current=0;
+      
       if (onSuccess) {
         onSuccess(result);
       }
+// pollInterval will happen only if API run was successful
+      if(pollInterval && !fetchSuccessful.current){
+        fetchSuccessful.current=setInterval(fetchResponse,pollInterval)
+      }
     } catch (e: any) {
-      setError(e);
-      if (onError) {
-        onError(e);
+      if (retryOnFailCount > retryCount.current) {
+        setTimeout(() => {
+         retryCount.current+=1;
+          fetchResponse();
+        }, retryOnFailInterval);
+      } else {
+        clearPoll();
+        setError(e);
+        if (onError) {
+          onError(e);
+        }
       }
     } finally {
       setLoading(false);
@@ -45,11 +76,12 @@ const useFetch = ({
     if (autoFetch) {
       fetchResponse();
     }
-    if (pollInterval) {
-      const val = setInterval(() => fetchResponse(), pollInterval);
-      return () => clearInterval(val);
-    }
+
+     return () => {
+      clearPoll();
+    };
   }, [url]);
+
 
   return {
     data,
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL: 'https://sample/api/',
+    baseURL: 'https://jsonplaceholder.typicode.com/',
     headers: {
         "Content-Type": "application/json",
       },
